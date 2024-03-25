3d08bff8dfaf977cfa8986ee3fb3de44b44f19f5
Sergey Budyakin <youshtuchka@gmail.com>
2024-03-25 22:55:41 +0300

    added evenOrOdd.js

---
+++ 8kyu/evenOrOdd.js
@@ -0,0 +1,18 @@
+// DESCRIPTION:
+//     Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+let numm = 4;
+function evenOrOdd(number) {
+    if (number % 2 === 0) {
+        return "Even";
+    } else {
+        return "Odd";
+    }
+}
+
+console.log(evenOrOdd(numm));
+// let result = evenOrOdd(numm)
+// console.log(result);
+
+
+
