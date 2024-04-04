1746ebce6615619e6588aa8274d0cc6f93e82595
Sergey Budyakin <youshtuchka@gmail.com>
2024-04-04 18:42:14 +0300

    Create OppositeNumber.js

---
+++ 8kyu/OppositeNumber.js
@@ -0,0 +1,8 @@
+// Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+let num = 2
+
+function opposite(number) {
+    return number * -1;
+}
+console.log(opposite(num));
