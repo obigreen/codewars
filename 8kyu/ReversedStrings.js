6f7334454d26972425b662680a0cd30294ad71e2
Sergey Budyakin <youshtuchka@gmail.com>
2024-05-15 19:40:54 +0300

    coplited the task

---
+++ 8kyu/ReversedStrings.js
@@ -0,0 +1,12 @@
+// Complete the solution so that it reverses the string passed into it.
+
+//'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+
+solution = (str) => str.split("").reverse().join("")
+
+// best practices, так я вначале сделал
+// function solution(str){
+//     return str.split('').reverse().join('');
+// }
