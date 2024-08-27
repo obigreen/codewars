848f72d57ce9268dd78c09b9571d2a13a7c07fea
Sergey Budyakin <youshtuchka@gmail.com>
2024-08-27 21:55:33 +0300

    +

---
+++ 7kyu/GrowthOfAPopulation.js
@@ -0,0 +1,18 @@
+// In a small town the population is p0 = 1000 at the beginning of a year. The population regularly increases by 2 percent per year and moreover 50 new inhabitants per year come to live in the town. How many years does the town need to see its population greater than or equal to p = 1200 inhabitants?
+
+
+// At the end of the first year there will be:
+// 1000 + 1000 * 0.02 + 50 => 1070 inhabitants
+//
+// At the end of the 2nd year there will be:
+// 1070 + 1070 * 0.02 + 50 => 1141 inhabitants (** number of inhabitants is an integer **)
+//
+// At the end of the 3rd year there will be:
+// 1141 + 1141 * 0.02 + 50 => 1213
+//
+// It will need 3 entire years.
+
+
+function nbYear(p0, percent, aug, p) {
+
+}
